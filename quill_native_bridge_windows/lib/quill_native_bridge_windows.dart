d40f9245e2630a7e427f5cd3ba162ec866372682
bhaveshprodev <114125028+bhaveshprodev@users.noreply.github.com>
2025-08-06 14:11:47 +0530

    Update quill_native_bridge_windows.dart

--- quill_native_bridge_windows/lib/quill_native_bridge_windows.dart
+++ quill_native_bridge_windows/lib/quill_native_bridge_windows.dart
@@ -44,146 +44,148 @@ class QuillNativeBridgeWindows extends QuillNativeBridgePlatform {
   /// Refer to [Windows GetClipboardData() docs](https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getclipboarddata)
   @override
   Future<String?> getClipboardHtml() async {
-    if (OpenClipboard(NULL) == FALSE) {
-      assert(
-        false,
-        'Unknown error while opening the clipboard. Error code: ${GetLastError()}',
-      );
-      return null;
-    }
+    return '';
+    
+    // if (OpenClipboard(NULL) == FALSE) {
+    //   assert(
+    //     false,
+    //     'Unknown error while opening the clipboard. Error code: ${GetLastError()}',
+    //   );
+    //   return null;
+    // }
 
-    try {
-      final htmlFormatId = cfHtml;
+    // try {
+    //   final htmlFormatId = cfHtml;
 
-      if (htmlFormatId == null) {
-        assert(false, 'Failed to register clipboard HTML format.');
-        return null;
-      }
+    //   if (htmlFormatId == null) {
+    //     assert(false, 'Failed to register clipboard HTML format.');
+    //     return null;
+    //   }
 
-      if (IsClipboardFormatAvailable(htmlFormatId) == FALSE) {
-        return null;
-      }
+    //   if (IsClipboardFormatAvailable(htmlFormatId) == FALSE) {
+    //     return null;
+    //   }
 
-      final clipboardDataHandle = GetClipboardData(htmlFormatId);
-      if (clipboardDataHandle == NULL) {
-        assert(
-          false,
-          'Failed to get clipboard data. Error code: ${GetLastError()}',
-        );
-        return null;
-      }
+    //   final clipboardDataHandle = GetClipboardData(htmlFormatId);
+    //   if (clipboardDataHandle == NULL) {
+    //     assert(
+    //       false,
+    //       'Failed to get clipboard data. Error code: ${GetLastError()}',
+    //     );
+    //     return null;
+    //   }
 
-      final clipboardDataPointer = Pointer.fromAddress(clipboardDataHandle);
-      final lockedMemoryPointer = GlobalLock(clipboardDataPointer);
-      if (lockedMemoryPointer == nullptr) {
-        assert(
-          false,
-          'Failed to lock global memory. Error code: ${GetLastError()}',
-        );
-        return null;
-      }
+    //   final clipboardDataPointer = Pointer.fromAddress(clipboardDataHandle);
+    //   final lockedMemoryPointer = GlobalLock(clipboardDataPointer);
+    //   if (lockedMemoryPointer == nullptr) {
+    //     assert(
+    //       false,
+    //       'Failed to lock global memory. Error code: ${GetLastError()}',
+    //     );
+    //     return null;
+    //   }
 
-      final windowsHtmlWithMetadata =
-          lockedMemoryPointer.cast<Utf8>().toDartString();
-      GlobalUnlock(clipboardDataPointer);
+    //   final windowsHtmlWithMetadata =
+    //       lockedMemoryPointer.cast<Utf8>().toDartString();
+    //   GlobalUnlock(clipboardDataPointer);
 
-      // Strip comments/headers at the start of the HTML as they can cause
-      // issues while parsing the HTML
+    //   // Strip comments/headers at the start of the HTML as they can cause
+    //   // issues while parsing the HTML
 
-      final cleanedHtml =
-          stripWindowsHtmlDescriptionHeaders(windowsHtmlWithMetadata);
+    //   final cleanedHtml =
+    //       stripWindowsHtmlDescriptionHeaders(windowsHtmlWithMetadata);
 
-      return cleanedHtml;
-    } finally {
-      CloseClipboard();
-    }
+    //   return cleanedHtml;
+    // } finally {
+    //   CloseClipboard();
+    // }
   }
 
   /// Refer to [Windows SetClipboardData() docs](https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-setclipboarddata)
   @override
   Future<void> copyHtmlToClipboard(String html) async {
-    if (OpenClipboard(NULL) == FALSE) {
-      assert(
-        false,
-        'Unknown error while opening the clipboard. Error code: ${GetLastError()}',
-      );
-      return;
-    }
+    // if (OpenClipboard(NULL) == FALSE) {
+    //   assert(
+    //     false,
+    //     'Unknown error while opening the clipboard. Error code: ${GetLastError()}',
+    //   );
+    //   return;
+    // }
 
-    final windowsClipboardHtml = constructWindowsHtmlDescriptionHeaders(html);
-    final htmlPointer = windowsClipboardHtml.toNativeUtf8();
+    // final windowsClipboardHtml = constructWindowsHtmlDescriptionHeaders(html);
+    // final htmlPointer = windowsClipboardHtml.toNativeUtf8();
 
-    try {
-      if (EmptyClipboard() == FALSE) {
-        assert(
-          false,
-          'Failed to empty the clipboard. Error code: ${GetLastError()}',
-        );
-        return;
-      }
+    // try {
+    //   if (EmptyClipboard() == FALSE) {
+    //     assert(
+    //       false,
+    //       'Failed to empty the clipboard. Error code: ${GetLastError()}',
+    //     );
+    //     return;
+    //   }
 
-      final htmlFormatId = cfHtml;
+    //   final htmlFormatId = cfHtml;
 
-      if (htmlFormatId == null) {
-        assert(
-          false,
-          'Failed to register clipboard HTML format. Error code: ${GetLastError()}',
-        );
-        return;
-      }
+    //   if (htmlFormatId == null) {
+    //     assert(
+    //       false,
+    //       'Failed to register clipboard HTML format. Error code: ${GetLastError()}',
+    //     );
+    //     return;
+    //   }
 
-      final unitSize = sizeOf<Uint8>();
-      final htmlSize = (htmlPointer.length + 1) * unitSize;
+    //   final unitSize = sizeOf<Uint8>();
+    //   final htmlSize = (htmlPointer.length + 1) * unitSize;
 
-      final clipboardMemoryHandle = GlobalAlloc(GMEM_MOVEABLE, htmlSize);
-      if (clipboardMemoryHandle == nullptr) {
-        assert(
-          false,
-          'Failed to allocate memory for the clipboard content. Error code: ${GetLastError()}',
-        );
-        return;
-      }
+    //   final clipboardMemoryHandle = GlobalAlloc(GMEM_MOVEABLE, htmlSize);
+    //   if (clipboardMemoryHandle == nullptr) {
+    //     assert(
+    //       false,
+    //       'Failed to allocate memory for the clipboard content. Error code: ${GetLastError()}',
+    //     );
+    //     return;
+    //   }
 
-      final lockedMemoryPointer = GlobalLock(clipboardMemoryHandle);
-      if (lockedMemoryPointer == nullptr) {
-        GlobalFree(clipboardMemoryHandle);
-        assert(
-          false,
-          'Failed to lock global memory. Error code: ${GetLastError()}',
-        );
-        return;
-      }
+    //   final lockedMemoryPointer = GlobalLock(clipboardMemoryHandle);
+    //   if (lockedMemoryPointer == nullptr) {
+    //     GlobalFree(clipboardMemoryHandle);
+    //     assert(
+    //       false,
+    //       'Failed to lock global memory. Error code: ${GetLastError()}',
+    //     );
+    //     return;
+    //   }
 
-      final targetMemoryPointer = lockedMemoryPointer.cast<Uint8>();
+    //   final targetMemoryPointer = lockedMemoryPointer.cast<Uint8>();
 
-      final sourcePointer = htmlPointer.cast<Uint8>();
+    //   final sourcePointer = htmlPointer.cast<Uint8>();
 
-      // Copy HTML data byte by byte
-      for (var i = 0; i < htmlPointer.length; i++) {
-        targetMemoryPointer[i] = (sourcePointer + i).value;
-      }
+    //   // Copy HTML data byte by byte
+    //   for (var i = 0; i < htmlPointer.length; i++) {
+    //     targetMemoryPointer[i] = (sourcePointer + i).value;
+    //   }
 
-      // Add a null terminator for HTML (necessary for proper string handling)
-      (targetMemoryPointer + htmlPointer.length).value = NULL;
+    //   // Add a null terminator for HTML (necessary for proper string handling)
+    //   (targetMemoryPointer + htmlPointer.length).value = NULL;
 
-      // According to Windows docs in https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-setclipboarddata#parameters
-      // Should not call GlobalFree() when SetClipboardData() success
-      // as the Windows clipboard takes ownership of the memory.
+    //   // According to Windows docs in https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-setclipboarddata#parameters
+    //   // Should not call GlobalFree() when SetClipboardData() success
+    //   // as the Windows clipboard takes ownership of the memory.
 
-      GlobalUnlock(clipboardMemoryHandle);
+    //   GlobalUnlock(clipboardMemoryHandle);
 
-      if (SetClipboardData(htmlFormatId, clipboardMemoryHandle.address) ==
-          NULL) {
-        GlobalFree(clipboardMemoryHandle);
-        assert(
-          false,
-          'Failed to set the clipboard data: ${GetLastError()}',
-        );
-      }
-    } finally {
-      CloseClipboard();
-      calloc.free(htmlPointer);
-    }
+    //   if (SetClipboardData(htmlFormatId, clipboardMemoryHandle.address) ==
+    //       NULL) {
+    //     GlobalFree(clipboardMemoryHandle);
+    //     assert(
+    //       false,
+    //       'Failed to set the clipboard data: ${GetLastError()}',
+    //     );
+    //   }
+    // } finally {
+    //   CloseClipboard();
+    //   calloc.free(htmlPointer);
+    // }
   }
 
   @visibleForTesting
